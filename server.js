c5cbc54d1ccfeee6de7d71db30c14c8f535c83b6
christopherknightmerritt <chris.merritt95@gmail.com>
2019-01-28 15:28:46 -0800

    function login system

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override');
 
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
-const client = new pg.Client(process.envDATABASE_URL);
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('err', err=>console.log(err));
 
@@ -29,13 +29,39 @@ app.use(
   })
 );
 app.get('/', loadLogin);
-app.get('/dashboard', getLocation);
+// app.get('/dashboard', getLocation);
+app.get('/dashboard', checkPassword);
+app.post('/dashboard', loadDashboard);
 
 function loadLogin (request, response) {
   response.render('./index', {formaction: 'get'});
 }
 
-function getLocation(request, response) {
-  response.render('./pages/dashboard');
+function loadDashboard(request, response) {
+  let {username, password} = request.body;
+  let SQL = `INSERT INTO users(username, password) VALUES ($1, $2);`;
+  let values = [username, password];
+  return client.query(SQL, values)
+  .then(response.render('./pages/dashboard'))
 }
 
+function getLocation (request, response) {
+  response.render('./pages/dashboard');
+}
+function checkPassword (request, response){
+  let SQL = `SELECT * FROM users WHERE username=$1 AND password=$2;`;
+  let values = [request.query.username, request.query.password];
+
+  client.query(SQL, values)
+  .then(result => {
+    if(result.rows.length > 0){
+      response.render('./pages/dashboard');
+    }
+    else{
+      response.render('./index')
+    }
+  })
+  .catch( () => {
+    response.render('./index')
+  });
+}
