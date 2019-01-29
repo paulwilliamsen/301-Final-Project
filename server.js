7ef8e28f5e8394e1d40937cf2728a23c184148c1
Jessica Roland <jroland015004@gmail.com>
2019-01-28 16:56:26 -0800

    Location query added

--- server.js
+++ server.js
@@ -29,15 +29,15 @@ app.use(
   })
 );
 app.get('/', loadLogin);
-// app.get('/dashboard', getLocation);
 app.get('/dashboard', checkPassword);
 app.post('/dashboard', loadDashboard);
+app.post('/location', findLocation);
 
 
 //error handler
 function errorHandler(err, response){
-	console.error(err);
-	if(response) response.status(500).send('Something Broke!!!')
+  console.error(err);
+  if(response) response.status(500).send('Something Broke!!!')
 }
 
 function loadDashboard(request, response) {
@@ -45,28 +45,31 @@ function loadDashboard(request, response) {
   let SQL = `INSERT INTO users(username, password) VALUES ($1, $2);`;
   let values = [username, password];
   return client.query(SQL, values)
-  .then(response.render('./pages/dashboard'))
+    .then(response.render('./pages/dashboard'))
 }
 
 function getLocation(request, response){
-	let SQL =`SELECT locations.formatted_query, locations.latitude, locations.longitude FROM locations WHERE location_id=$1 `
-	let values = [locationHandler.query.id];
-	return client.query(SQL, values);
+  let SQL =`SELECT locations.formatted_query, locations.latitude, locations.longitude FROM locations WHERE location_id=$1 `
+  let values = [locationHandler.query.id];
+  return client.query(SQL, values);
 }
 
 function findLocation(request, response){
+	console.log(request.body.search);
   const locationHandler = {
 
-    query: request.query.data,
+    query: request.body.search,
 
-    // cacheHit:
+    cacheHit: (results)=>{
+      response.send(results.rows[0]);
+    },
 
     cacheMiss: ()=>{
-      Location.fetchLocation(request.query.data)
-        .then(data=>response.send(data))
+      Location.fetchLocation(locationHandler.query)
+        .then(data=>response.render('pages/dashboard', {data}));
     }
-	}
-	Location.lookupLocation(locationHandler);
+  };
+  Location.lookupLocation(locationHandler);
 }
 
 function checkPassword (request, response){
@@ -74,65 +77,66 @@ function checkPassword (request, response){
   let values = [request.query.username, request.query.password];
 
   client.query(SQL, values)
-  .then(result => {
-    if(result.rows.length > 0){
-      response.render('./pages/dashboard');
-    }
-    else{
+    .then(result => {
+      if(result.rows.length > 0){
+        response.render('./pages/dashboard', {data:'No data yet'});
+      }
+      else{
+        response.render('./index')
+      }
+    })
+    .catch( () => {
       response.render('./index')
-    }
-  })
-  .catch( () => {
-    response.render('./index')
-  });
+    });
 }
 
-Location.fetchLocation(query){
-	const geoData = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
-	return superagent.get(geoData)
-		.then(response=>{
-			if(!response.body(data)){
-				throw 'no data';
-			}
-			else{
+Location.fetchLocation = (query)=>{
+  const geoData = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+  return superagent.get(geoData)
+    .then(response=>{
+      if(!response.body.results.length){
+        throw 'no data';
+      }
+      else{
 				let location = new Location(query, response.body.results[0])
-				return location.save()
-					.then(result =>{
-						location.id = result.rows[0].id;
-						return location;
-					})
-			}
-		})
-		.catch(error => errorHandler(error));
+				console.log(location);
+        return location.save()
+          .then(result =>{
+            location.id = result.rows[0].id;
+            return location;
+          })
+      }
+    })
+    .catch(error => errorHandler(error));
 }
 
 function Location(query, response){
-	this.formatted_query = response.formatted_address; 
-	this.latitude = response.geometry.location.lat;
-	this.longitude = response.geometry.location.lng;
-	this.search_query = query;
+  this.formatted_query = response.formatted_address; 
+  this.latitude = response.geometry.location.lat;
+  this.longitude = response.geometry.location.lng;
+  this.search_query = query;
 }
 
-Location.prototype.save(){
-	let SQL = `INSERT INTO locations (formatted_query, latitude. longitude, search_query) VALUES ($1, $2, $3, $4) RETURNING id;`;
-	let values =[this.formatted_query, this.latitude, this.longitude, this.search_query];
-	return client.query(SQL, values);
+Location.prototype.save = function(){
+  let SQL = `INSERT INTO locations (formatted_query, latitude, longitude, search_query) VALUES ($1, $2, $3, $4) RETURNING id;`;
+  let values =[this.formatted_query, this.latitude, this.longitude, this.search_query];
+  return client.query(SQL, values);
 }
 
-Location.lookupLocation = (handler){
-	const SQL = `SELECT * FROM locations WHERE search_query=$1`
-	const values = [handler.query];
+Location.lookupLocation = (handler)=>{
+  const SQL = `SELECT * FROM locations WHERE search_query=$1`
+  const values = [handler.query];
 
-	return client.query(SQL, values)
-		.then(results=>{
-			if(results.rowCount > 0){
-				handler.cacheHit(results);
-			}
-			else{
-				handler.cacheMiss();
-			}
-		})
-		.catch(console.error);
+  return client.query(SQL, values)
+    .then(results=>{
+      if(results.rowCount > 0){
+        handler.cacheHit(results);
+      }
+      else{
+        handler.cacheMiss();
+      }
+    })
+    .catch(console.error);
 };
 
 function loadLogin (request, response) {
